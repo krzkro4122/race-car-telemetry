60d92aa3e487a630daf65c2e66c901d0d9490e58
krzkro4122 <krolkrzysztof1999@gmail.com>
2021-04-24 12:28:42 +0200

    Data for every channel (button) separate ScrollArea implementation for labels Dynamic Data label creation according to Data.txt contents Labels appearance altered slightly Program starts displaying graph for channel of button 12 TODO: QtTimer implementation

--- main.py
+++ main.py
@@ -3,41 +3,21 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import Qt
 
-# from pyqtgraph import PlotWidget, plot
 import pyqtgraph as pg
-# import pandas as pd
-
+import pandas as pd
 import sys  # We need sys so that we can pass argv to QApplication
 import random
-# import os
+# from multiprocessing import *
 
-NAMES = ["Velocity", "RPM", "Oil Temperature", "Battery Voltage",
-         "Battery Temperature", "Mileage", "Something else"]
-VALUES = {
-    "Velocity": random.randint(0, 250),
-    "RPM": random.randint(0, 8000),
-    "Oil Temperature": random.randint(0, 120),
-    "Battery Voltage": random.randint(10, 50),
-    "Battery Temperature": random.randint(0, 80),
-    "Mileage": random.randint(0, 5000),
-    "Something else": random.randint(0, 12)
-}
-UNITS = {
-    "Velocity": "km/h",
-    "RPM": "rev/min",
-    "Oil Temperature": "°C",
-    "Battery Voltage": "V",
-    "Battery Temperature": "°C",
-    "Mileage": "km",
-    "Something else": "lol/s"
-}
-# Some static data
-hour = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-temperature = [30, 32, 34, 32, 33, 31, 29, 32, 35, 45]
+# Data Dict
+# Format = {key: [values], ...}
+DATA = {line[0:len(line) - 1]: [random.randint(0, 1000) for i in range(100)] for line in open("Dane.txt")}
 
 
 # Main window af the application
 class MainWindow(QtWidgets.QMainWindow):
+
+    line: pg.plot
 
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
@@ -47,9 +27,10 @@ class MainWindow(QtWidgets.QMainWindow):
         self.graphWidget = pg.PlotWidget()
         self.mainFrame = QFrame()
         self.labelFrame = QFrame()
+        self.labelScroll = QScrollArea()
         self.pen = pg.mkPen(color=(255, 255, 255))  # Plot color
-        self.line = 0
-        self.lastButtonClicked = 0
+        self.lastButtonClicked = 0  # Last clicked button's index (so it can be uncolored)
+        self.currentDataKey = list(DATA.keys())[0]
         self.valueLabelContainer = []
         self.valueButtonContainer = []
         # Run GUI
@@ -72,67 +53,76 @@ class MainWindow(QtWidgets.QMainWindow):
     # Builds a label grid
     def makeLabels(self):
         # Add to mainWindow's grid
-        self.grid.addWidget(self.labelFrame, 0, 0)
+        self.grid.addWidget(self.labelScroll, 0, 0)
         self.labelFrame.setLayout(self.gridOfLabels)
-
+        # Scroll area's settings
+        self.labelScroll.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        self.labelScroll.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.labelScroll.setWidgetResizable(True)
+        self.labelScroll.setMaximumWidth(500)
+        self.labelScroll.setMinimumWidth(500)
+        self.labelScroll.setWidget(self.labelFrame)
         # First row is static - Annotates columns
         self.gridOfLabels.addWidget(QLabel(text="Name"), 0, 1)
-        self.gridOfLabels.addWidget(QLabel(text="Value"), 0, 2)
+        self.gridOfLabels.addWidget(QLabel(text="Value"), 0, 2, alignment=Qt.AlignCenter)
         self.gridOfLabels.addWidget(QLabel(text="Unit"), 0, 3)
         # Create labels
-        for i in range(6):
-            for index, name in enumerate(NAMES):
-                row = index + i * len(NAMES) + 1
-                # PLOT BUTTONS
-                plotButton = QPushButton(text=f"{row}")  # Create
-                plotButton.setFixedWidth(20)  # Buttons smaller
-                self.gridOfLabels.addWidget(plotButton, row, 0, alignment=Qt.AlignLeft)  # Locate in grid
-                self.gridOfLabels.setSpacing(1)  # Makes button text readable
-                # NAMES
-                nameLabel = QtWidgets.QLabel(text=f"{name}")  # Create
-                nameLabel.setFont(QFont("Segoe UI", 8))  # Change font size
-                self.gridOfLabels.addWidget(nameLabel, row, 1)  # Locate in grid
-                # VALUES
-                valueLabel = QtWidgets.QLabel(text=str(VALUES[name]))
-                valueLabel.setFont(QFont("Segoe UI", 9))
-                self.gridOfLabels.addWidget(valueLabel, row, 2)
-                # UNITS
-                metricLabel = QtWidgets.QLabel(text=UNITS[name])
-                metricLabel.setFont(QFont("Segoe UI", 8))
-                self.gridOfLabels.addWidget(metricLabel, row, 3)
-                # Store value labels for further text manipulation
-                self.valueLabelContainer.append(valueLabel)
-                # Store index buttons for further callback assignment
-                self.valueButtonContainer.append(plotButton)
-                self.valueButtonContainer[row - 1].clicked.connect(lambda state, x=row: self.changeColor(x))
+        names = list(DATA.keys())
+        for index, name in enumerate(names):
+            row = index + 1
+            # PLOT BUTTONS
+            plotButton = QPushButton(text=f"{row}")  # Create
+            plotButton.setFixedWidth(20)  # Buttons smaller
+            self.gridOfLabels.addWidget(plotButton, row, 0, 1, 6, alignment=Qt.AlignLeft)  # Locate in grid
+            self.gridOfLabels.setSpacing(1)  # Makes button text readable
+            # NAMES
+            nameLabel = QtWidgets.QLabel(text=f"{name}")  # Create
+            nameLabel.setFont(QFont("Segoe UI", 8))  # Change font size
+            self.gridOfLabels.addWidget(nameLabel, row, 1, alignment=Qt.AlignLeft)  # Locate in grid
+            # VALUES
+            valueLabel = QtWidgets.QLabel(text=str(list(DATA.values())[index][-1]))
+            valueLabel.setFont(QFont("Segoe UI", 10))
+            valueLabel.setStyleSheet("border: 1px solid black;")
+            valueLabel.setFixedWidth(100)
+            valueLabel.setAlignment(Qt.AlignRight)
+            self.gridOfLabels.addWidget(valueLabel, row, 2)
+            # UNITS
+            metricLabel = QtWidgets.QLabel(text="meters idk")
+            metricLabel.setFont(QFont("Segoe UI", 8))
+            self.gridOfLabels.addWidget(metricLabel, row, 3, alignment=Qt.AlignLeft)
+            # Store value labels for further text manipulation
+            self.valueLabelContainer.append(valueLabel)
+            # Store index buttons for further callback assignment
+            self.valueButtonContainer.append(plotButton)
+            self.valueButtonContainer[row - 1].clicked.connect(lambda state, x=row: self.changeColor(x))
 
     # Builds Plot
     def makeGraphs(self):
         # Add to mainWindow's grid
         self.grid.addWidget(self.graphWidget, 0, 1)
         # plot data: x, y values
-        self.line = self.graphWidget.plot(hour, temperature)
+        gd = getData(self.currentDataKey)
+        self.line = self.graphWidget.plot([i for i in range(len(gd))], gd)
+        self.changeColor(12)  # start with button 12 active (Motor RPM)
 
     # Changes color of the clicked button as well as data and it's color on plot
     def changeColor(self, index):
         self.valueButtonContainer[self.lastButtonClicked].setStyleSheet("background-color: light gray")
         self.valueButtonContainer[index - 1].setStyleSheet("background-color: red")
         self.lastButtonClicked = index - 1
+        self.currentDataKey = list(DATA.keys())[index - 1]
         self.pen = pg.mkPen(color=(255, 0, 0))
-        self.updateData()  # For now - TODO - Multiprocess it
-
-    def updateData(self):
-        gd = getData()
-        self.line.setData([i for i in range(len(gd))], gd)  # line refresh with new data
-        self.line.setPen(self.pen)  # line color
+        updateData(self.line, self.currentDataKey)
 
 
-def getData():
-    # d = {name: [VALUES[name], UNITS[name]] for name in NAMES}  # Cool but not needed
-    d = [VALUES[name] for name in NAMES]
-    d *= 6
-    print(d)
-    return d
+def updateData(line, currentDataKey):
+    gd = getData(currentDataKey)
+    line.setData([i for i in range(len(gd))], gd)  # line refresh with new data
+
+
+def getData(currentDataKey):
+    df = pd.DataFrame(data=DATA)
+    return df.get(currentDataKey)
 
 
 def BuildUI():
@@ -143,7 +133,6 @@ def BuildUI():
 
 
 def main():
-    getData()
     BuildUI()
 
 
